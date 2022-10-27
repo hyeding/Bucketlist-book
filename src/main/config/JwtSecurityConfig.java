2211b0205c9026190a920f92c1072da48177a4ce
hyeding <65836744+hyeding@users.noreply.github.com>
2022-10-27 20:08:29 +0900

    feat: 기본적인 로그인 기능구현

---
+++ src/main/config/JwtSecurityConfig.java
@@ -0,0 +1,23 @@
+package TockTiMan.config;
+
+import TockTiMan.jwt.JwtFilter;
+import TockTiMan.jwt.TokenProvider;
+import lombok.RequiredArgsConstructor;
+import org.springframework.data.redis.core.StringRedisTemplate;
+import org.springframework.security.config.annotation.SecurityConfigurerAdapter;
+import org.springframework.security.config.annotation.web.builders.HttpSecurity;
+import org.springframework.security.web.DefaultSecurityFilterChain;
+import org.springframework.security.web.authentication.UsernamePasswordAuthenticationFilter;
+
+@RequiredArgsConstructor
+public class JwtSecurityConfig extends SecurityConfigurerAdapter<DefaultSecurityFilterChain, HttpSecurity> {
+
+    private final TokenProvider tokenProvider;
+    private final StringRedisTemplate redisTemplate;
+
+    @Override
+    public void configure(HttpSecurity http) {
+        JwtFilter customFilter = new JwtFilter(tokenProvider, redisTemplate);
+        http.addFilterBefore(customFilter, UsernamePasswordAuthenticationFilter.class);
+    }
+}
