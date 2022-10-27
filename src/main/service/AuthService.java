f0a9a913201e098e7678c5c3389266902cde58b5
hyeding <65836744+hyeding@users.noreply.github.com>
2022-10-27 20:11:38 +0900

    feat: 로그인 서비스단

---
+++ src/main/service/AuthService.java
@@ -0,0 +1,94 @@
+package TockTiMan.service;
+
+import TockTiMan.domain.*;
+import TockTiMan.dto.MemberRequestDto;
+import TockTiMan.dto.MemberResponseDto;
+import TockTiMan.dto.TokenDto;
+import TockTiMan.dto.TokenRequestDto;
+import TockTiMan.jwt.TokenProvider;
+import lombok.RequiredArgsConstructor;
+import org.springframework.security.authentication.UsernamePasswordAuthenticationToken;
+import org.springframework.security.config.annotation.authentication.builders.AuthenticationManagerBuilder;
+import org.springframework.security.core.Authentication;
+import org.springframework.security.crypto.password.PasswordEncoder;
+import org.springframework.stereotype.Service;
+import org.springframework.transaction.annotation.Transactional;
+
+import java.util.HashSet;
+import java.util.Set;
+
+@Service
+@RequiredArgsConstructor
+public class AuthService {
+    private final AuthenticationManagerBuilder authenticationManagerBuilder;
+    private final AuthorityRepository authorityRepository;
+    private final MemberRepository memberRepository;
+    private final PasswordEncoder passwordEncoder;
+    private final TokenProvider tokenProvider;
+    private final RefreshTokenRepository refreshTokenRepository;
+
+    /**
+     * 회원가입
+     */
+    @Transactional
+    public MemberResponseDto signup(MemberRequestDto memberRequestDto) {
+        if (memberRepository.existsByEmail(memberRequestDto.getEmail())) {
+            throw new RuntimeException("이미 가입되어 있는 이메일입니다.");
+        }
+
+        Authority authority = authorityRepository
+                .findByAuthorityStatus(AuthorityEnum.ROLE_USER).orElseThrow(()->new RuntimeException("권한 정보가 없습니다."));
+
+        Set<Authority> set = new HashSet<>();
+        set.add(authority);
+
+        Member member = memberRequestDto.toMember(passwordEncoder, set);
+        return MemberResponseDto.of(memberRepository.save(member));
+    }
+    /**
+     * 로그인
+     */
+    @Transactional
+    public TokenDto login(MemberRequestDto memberRequestDto) {
+        UsernamePasswordAuthenticationToken authenticationToken = memberRequestDto.toAuthentication();
+
+        Authentication authentication = authenticationManagerBuilder.getObject().authenticate(authenticationToken);
+
+        TokenDto tokenDto = tokenProvider.generateTokenDto(authentication);
+
+        RefreshToken refreshToken = RefreshToken.builder()
+                .key(authentication.getName())
+                .value(tokenDto.getRefreshToken())
+                .build();
+
+        refreshTokenRepository.save(refreshToken);
+
+        return tokenDto;
+    }
+
+    /**
+     * 재발급
+     */
+    @Transactional
+    public TokenDto reissue(TokenRequestDto tokenRequestDto) {
+        if (!tokenProvider.validateToken(tokenRequestDto.getRefreshToken())) {
+            throw new RuntimeException("Refresh Token 이 유효하지 않습니다.");
+        }
+
+        Authentication authentication = tokenProvider.getAuthentication(tokenRequestDto.getAccessToken());
+
+        RefreshToken refreshToken = refreshTokenRepository.findByKey(authentication.getName())
+                .orElseThrow(() -> new RuntimeException("로그아웃 된 사용자입니다."));
+
+        if (!refreshToken.getValue().equals(tokenRequestDto.getRefreshToken())) {
+            throw new RuntimeException("토큰의 유저 정보가 일치하지 않습니다.");
+        }
+
+        TokenDto tokenDto = tokenProvider.generateTokenDto(authentication);
+
+        RefreshToken newRefreshToken = refreshToken.updateValue(tokenDto.getRefreshToken());
+        refreshTokenRepository.save(newRefreshToken);
+
+        return tokenDto;
+    }
+}
