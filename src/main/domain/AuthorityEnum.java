73be017c03406d4925e50db9aa8b3290ae9c2744
hyeding <65836744+hyeding@users.noreply.github.com>
2022-11-03 18:40:08 +0900

    Delete src directory

--- src/main/domain/AuthorityEnum.java
+++
@@ -1,5 +0,0 @@
-package TockTiMan.domain;
-
-public enum AuthorityEnum {
-    ROLE_USER, ROLE_ADMIN
-}
--- src/main/domain/AuthorityRepository.java
+++
@@ -1,9 +0,0 @@
-package TockTiMan.domain;
-
-import org.springframework.data.jpa.repository.JpaRepository;
-
-import java.util.Optional;
-
-public interface AuthorityRepository extends JpaRepository<Authority, AuthorityEnum> {
-    Optional<Authority> findByAuthorityStatus(AuthorityEnum authorityStatus);
-}
