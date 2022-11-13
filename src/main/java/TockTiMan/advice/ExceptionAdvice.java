309602647b9c2e5829ff5b7f5559b97873cd0de8
hyeding <65836744+hyeding@users.noreply.github.com>
2022-11-13 11:09:43 +0900

    feat: 에러 응답

--- src/main/java/TockTiMan/advice/ExceptionAdvice.java
+++ src/main/java/TockTiMan/advice/ExceptionAdvice.java
@@ -1,10 +1,16 @@
 package TockTiMan.advice;
 
+import TockTiMan.exception.*;
+import TockTiMan.response.Response;
 import lombok.extern.slf4j.Slf4j;
 import org.springframework.http.HttpStatus;
+import org.springframework.validation.BindException;
+import org.springframework.web.bind.MethodArgumentNotValidException;
 import org.springframework.web.bind.annotation.ExceptionHandler;
 import org.springframework.web.bind.annotation.ResponseStatus;
 import org.springframework.web.bind.annotation.RestControllerAdvice;
+
+import javax.management.relation.RoleNotFoundException;
 
 @RestControllerAdvice
 @Slf4j
@@ -12,7 +18,7 @@ public class ExceptionAdvice {
     // 500 에러
     @ExceptionHandler(IllegalArgumentException.class) // 지정한 예외가 발생하면 해당 메소드 실행
     @ResponseStatus(HttpStatus.INTERNAL_SERVER_ERROR) // 각 예외마다 상태코드 지정
-    public Response illegalArgumentExceptionAdvice(IllegalArgumentExcepion e) {
+    public Response illegalArgumentExceptionAdvice(IllegalArgumentException e) {
         log.info("e = {}", e.getMessage());
         return Response.failure(500, e.getMessage().toString());
     }
@@ -27,7 +33,7 @@ public class ExceptionAdvice {
     }
 
     // 400 에러
-    // 요청 객체의 validation을 수행할 때, MethodrgumentNotValidException이 발생
+    // 요청 객체의 validation 을 수행할 때, MethodrgumentNotValidException 이 발생
     // 각 검증 어노테이션 별로 지정해놨던 메세지를 응답
     @ExceptionHandler(MethodArgumentNotValidException.class)
     @ResponseStatus(HttpStatus.BAD_REQUEST)
@@ -61,7 +67,7 @@ public class ExceptionAdvice {
 
     // 401 응답
     // 유저 정보가 일치하지 않음
-    @ExceptionHandler(MemberNotEqualseException.class)
+    @ExceptionHandler(MemberNotEqualsException.class)
     @ResponseStatus(HttpStatus.UNAUTHORIZED)
     public Response memberNotEqualsException() {
         return Response.failure(401, "유저 정보가 일치하지 않습니다.");
@@ -92,7 +98,7 @@ public class ExceptionAdvice {
     }
 
     // 404 응답
-    // 요청한 Comment를 찾을 수 없음
+    // 요청한 Comment 를 찾을 수 없음
     @ExceptionHandler(CommentNotFoundException.class)
     @ResponseStatus(HttpStatus.NOT_FOUND)
     public Response commentNotFoundException() {
@@ -111,7 +117,7 @@ public class ExceptionAdvice {
     // 파일 업로드 실패
     @ExceptionHandler(FileUploadFailureException.class)
     @ResponseStatus(HttpStatus.NOT_FOUND)
-    public Response fileUploadFailureException(fileUploadFailureException e) {
+    public Response fileUploadFailureException(FileUploadFailureException e) {
         log.error("e = {}", e.getMessage());
         return Response.failure(404, "이미지 업로드 실패");
     }
@@ -144,7 +150,7 @@ public class ExceptionAdvice {
     // nickname 중복
     @ExceptionHandler(MemberNicknameAlreadyExistsException.class)
     @ResponseStatus(HttpStatus.CONFLICT)
-    public Response memberNicknameAlreadyExistsException(MemberNicknameAlredayExistsException e) {
+    public Response memberNicknameAlreadyExistsException(MemberNicknameAlreadyExistsException e) {
         return Response.failure(409, e.getMessage() + "은 중복된 닉네임 입니다.");
     }
 }
