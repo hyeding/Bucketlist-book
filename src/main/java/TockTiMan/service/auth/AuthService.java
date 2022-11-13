be823cbb6d80882cfbcd59935536edd5f3cdf173
hyeding <65836744+hyeding@users.noreply.github.com>
2022-11-13 11:20:09 +0900

    feat: 비즈니스 로직

---
+++ src/main/java/TockTiMan/service/auth/AuthService.java
@@ -0,0 +1,132 @@
+package TockTiMan.service.auth;
+
+import TockTiMan.config.jwt.TokenProvider;
+import TockTiMan.dto.sign.*;
+import TockTiMan.entity.user.Authority;
+import TockTiMan.entity.user.User;
+import TockTiMan.exception.LoginFailureException;
+import TockTiMan.exception.MemberNicknameAlreadyExistsException;
+import TockTiMan.exception.MemberUsernameAlreadyExistsException;
+import TockTiMan.repository.refreshToken.RefreshTokenRepository;
+import TockTiMan.repository.user.UserRepository;
+import TockTiMan.service.redis.RedisService;
+import lombok.RequiredArgsConstructor;
+import org.springframework.security.authentication.UsernamePasswordAuthenticationToken;
+import org.springframework.security.config.annotation.authentication.builders.AuthenticationManagerBuilder;
+import org.springframework.security.core.Authentication;
+import org.springframework.security.crypto.password.PasswordEncoder;
+import org.springframework.stereotype.Service;
+import org.springframework.transaction.annotation.Transactional;
+
+@Service
+@RequiredArgsConstructor
+public class AuthService {
+
+    private final AuthenticationManagerBuilder authenticationManagerBuilder;
+    private final UserRepository userRepository;
+    private final PasswordEncoder passwordEncoder;
+    private final TokenProvider tokenProvider;
+    private final RefreshTokenRepository refreshTokenRepository;
+    private final RedisService redisService;
+
+    @Transactional
+    public void signup(SignUpRequestDto req) {
+        validateSignUpInfo(req);
+
+        // Builder로 리팩토링 해야함
+        User user = new User();
+        user.setUsername(req.getUsername());
+        user.setPassword(passwordEncoder.encode(req.getPassword()));
+        user.setNickname(req.getNickname());
+        user.setName(req.getName());
+        user.setAuthority(Authority.ROLE_USER);
+        userRepository.save(user);
+    }
+
+
+    @Transactional
+    public TokenResponseDto signIn(LoginRequestDto req) {
+        User user = userRepository.findByUsername(req.getUsername()).orElseThrow(() -> {
+            return new LoginFailureException();
+        });
+
+        validatePassword(req, user);
+
+        // 1. Login ID/PW 를 기반으로 AuthenticationToken 생성
+        UsernamePasswordAuthenticationToken authenticationToken = req.toAuthentication();
+
+        // 2. 실제로 검증 (사용자 비밀번호 체크) 이 이루어지는 부분
+        //    authenticate 메서드가 실행이 될 때 CustomUserDetailsService 에서 만들었던 loadUserByUsername 메서드가 실행됨
+        Authentication authentication = authenticationManagerBuilder.getObject().authenticate(authenticationToken);
+
+        // 3. 인증 정보를 기반으로 JWT 토큰 생성
+        TokenDto tokenDto = tokenProvider.generateTokenDto(authentication);
+
+        // 4. RefreshToken 저장
+//        RefreshToken refreshToken = RefreshToken.builder()
+//                .key(authentication.getName())
+//                .value(tokenDto.getRefreshToken())
+//                .build();
+        redisService.setValues(authentication.getName(), tokenDto.getRefreshToken());
+
+//        refreshTokenRepository.save(refreshToken);
+        TokenResponseDto tokenResponseDto = new TokenResponseDto(tokenDto.getAccessToken(), tokenDto.getRefreshToken());
+        // 5. 토큰 발급
+        return tokenResponseDto;
+    }
+
+
+    @Transactional
+    public TokenResponseDto reissue(TokenRequestDto tokenRequestDto) {
+//        // 1. Refresh Token 검증
+//        if (!tokenProvider.validateToken(tokenRequestDto.getRefreshToken())) {
+//            throw new RuntimeException("Refresh Token 이 유효하지 않습니다.");
+//        }
+//
+//        // 2. Access Token 에서 Member ID 가져오기
+//        Authentication authentication = tokenProvider.getAuthentication(tokenRequestDto.getAccessToken());
+//
+//        // 3. 저장소에서 Member ID 를 기반으로 Refresh Token 값 가져옴
+//        RefreshToken refreshToken = refreshTokenRepository.findByKey(authentication.getName())
+//                .orElseThrow(() -> new RuntimeException("로그아웃 된 사용자입니다."));
+//
+//        // 4. Refresh Token 일치하는지 검사
+//        if (!refreshToken.getValue().equals(tokenRequestDto.getRefreshToken())) {
+//            throw new RuntimeException("토큰의 유저 정보가 일치하지 않습니다.");
+//        }
+//
+//        // 5. 새로운 토큰 생성
+//        TokenDto tokenDto = tokenProvider.generateTokenDto(authentication);
+//
+//        // 6. 저장소 정보 업데이트
+//        RefreshToken newRefreshToken = refreshToken.updateValue(tokenDto.getRefreshToken());
+//        refreshTokenRepository.save(newRefreshToken);
+//
+//        // 토큰 발급
+//        TokenResponseDto tokenResponseDto = new TokenResponseDto(tokenDto.getAccessToken(), tokenDto.getRefreshToken());
+//        return tokenResponseDto;
+
+        Authentication authentication = tokenProvider.getAuthentication(tokenRequestDto.getAccessToken());
+        redisService.checkRefreshToken(authentication.getName(), tokenRequestDto.getRefreshToken());
+
+        // 예외 처리 통과후 토큰 재생성
+        TokenDto tokenDto = tokenProvider.generateTokenDto(authentication);
+        TokenResponseDto tokenResponseDto = new TokenResponseDto(tokenDto.getAccessToken(), tokenDto.getRefreshToken());
+        return tokenResponseDto;
+    }
+
+
+    private void validateSignUpInfo(SignUpRequestDto signUpRequestDto) {
+        if (userRepository.existsByUsername(signUpRequestDto.getUsername()))
+            throw new MemberUsernameAlreadyExistsException(signUpRequestDto.getUsername());
+        if (userRepository.existsByNickname(signUpRequestDto.getNickname()))
+            throw new MemberNicknameAlreadyExistsException(signUpRequestDto.getNickname());
+    }
+
+    private void validatePassword(LoginRequestDto loginRequestDto, User user) {
+        if (!passwordEncoder.matches(loginRequestDto.getPassword(), user.getPassword())) {
+            throw new LoginFailureException();
+        }
+    }
+
+}
---
+++ src/main/java/TockTiMan/service/message/MessageService.java
@@ -0,0 +1,116 @@
+package TockTiMan.service.message;
+
+import TockTiMan.dto.message.MessageCreateRequest;
+import TockTiMan.dto.message.MessageDto;
+import TockTiMan.entity.message.Message;
+import TockTiMan.entity.user.User;
+import TockTiMan.exception.MemberNotEqualsException;
+import TockTiMan.exception.MemberNotFoundException;
+import TockTiMan.exception.MessageNotFoundException;
+import TockTiMan.repository.message.MessageRepository;
+import TockTiMan.repository.user.UserRepository;
+import lombok.RequiredArgsConstructor;
+import org.springframework.security.core.Authentication;
+import org.springframework.security.core.context.SecurityContextHolder;
+import org.springframework.stereotype.Service;
+import org.springframework.transaction.annotation.Transactional;
+
+import java.util.ArrayList;
+import java.util.List;
+
+@RequiredArgsConstructor
+@Service
+public class MessageService {
+
+    private final MessageRepository messageRepository;
+    private final UserRepository userRepository;
+
+    @Transactional
+    public MessageDto createMessage(User sender, MessageCreateRequest req) {
+        User receiver = userRepository.findByNickname(req.getReceiverNickname()).orElseThrow(MemberNotFoundException::new);
+        Message message = new Message(req.getTitle(), req.getContent(), sender, receiver);
+        return MessageDto.toDto(messageRepository.save(message));
+    }
+
+    @Transactional(readOnly = true)
+    public List<MessageDto> receiveMessages(User user) {
+
+        List<MessageDto> messageDtoList = new ArrayList<>();
+        List<Message> messageList = messageRepository.findAllByReceiverAndDeletedByReceiverFalseOrderByIdDesc(user);
+
+        for (Message message : messageList) {
+            messageDtoList.add(MessageDto.toDto(message));
+        }
+        return messageDtoList;
+    }
+
+
+    @Transactional(readOnly = true)
+    public MessageDto receiveMessage(int id, User user) {
+        Message message = messageRepository.findById(id).orElseThrow(MessageNotFoundException::new);
+
+        if (message.getReceiver() != user) {
+            throw new MemberNotEqualsException();
+        }
+        if (message.isDeletedByReceiver()) {
+            throw new MessageNotFoundException();
+        }
+        return MessageDto.toDto(message);
+    }
+
+    @Transactional(readOnly = true)
+    public List<MessageDto> sendMessages(User user) {
+        List<MessageDto> messageDtoList = new ArrayList<>();
+        List<Message> messageList = messageRepository.findAllBySenderAndDeletedBySenderFalseOrderByIdDesc(user);
+
+        for (Message message : messageList) {
+            messageDtoList.add(MessageDto.toDto(message));
+        }
+        return messageDtoList;
+    }
+
+    @Transactional(readOnly = true)
+    public MessageDto sendMessage(int id, User user) {
+        Message message = messageRepository.findById(id).orElseThrow(MessageNotFoundException::new);
+
+        if (message.getSender() != user) {
+            throw new MemberNotEqualsException();
+        }
+
+        if (message.isDeletedByReceiver()) {
+            throw new MessageNotFoundException();
+        }
+        return MessageDto.toDto(message);
+    }
+
+    @Transactional
+    public void deleteMessageByReceiver(int id, User user) {
+        Message message = messageRepository.findById(id).orElseThrow(MessageNotFoundException::new);
+
+        if (message.getReceiver() == user) {
+            message.deleteByReceiver();
+        } else {
+            throw new MemberNotEqualsException();
+        }
+
+        if (message.isDeletedMessage()) {
+            // 수신, 송신자 둘다 삭제할 경우
+            messageRepository.delete(message);
+        }
+    }
+
+    @Transactional
+    public void deleteMessageBySender(int id, User user) {
+        Message message = messageRepository.findById(id).orElseThrow(MessageNotFoundException::new);
+
+        if (message.getSender() == user) {
+            message.deleteBySender();
+        } else {
+            throw new MemberNotEqualsException();
+        }
+
+        if (message.isDeletedMessage()) {
+            messageRepository.delete(message);
+        }
+    }
+}
